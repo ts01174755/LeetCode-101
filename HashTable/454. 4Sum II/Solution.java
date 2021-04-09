b3633d357f8d8b49b8f76321056e664be27e623d
Wu,Pei-Yu <ts01174755@gmail.com>
2021-04-09 16:25:13 +0800

    Update Solution.java

--- HashTable/454. 4Sum II/Solution.java
+++ HashTable/454. 4Sum II/Solution.java
@@ -3,22 +3,14 @@ class Solution {
         Map<Integer, Integer> map = new HashMap<>();
         int count = 0;
         
-        for(int i = 0; i < A.length; i++) {
-        	for (int j = 0; j < B.length; j++) {
-        		int sum = A[i] + B[j];
-        		if (map.containsKey(sum)) {
-        			map.put(sum, map.get(sum)+1);
-        		} else {
-        			map.put(sum, 1);
-        		}
+        for(int a: A) {
+        	for (int b: B) {
+        		map.put(a + b, map.getOrDefault(a + b, 0)+1);
         	}
         }
-        for(int i = 0; i < C.length; i++) {
-        	for (int j = 0; j < D.length; j++) {
-        		int sum = -(C[i] + D[j]);
-        		if (map.containsKey(sum)) {
-        			count += map.get(sum);
-        		}
+        for(int c: C) {
+        	for (int d: D) {
+        		count += map.getOrDefault(-(c + d), 0);
         	}
         }
 		
