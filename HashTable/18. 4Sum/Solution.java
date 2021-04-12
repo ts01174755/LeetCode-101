0d2b7d1b2cd9da8cde428d6bd4c2bd251e094f39
Wu,Pei-Yu <ts01174755@gmail.com>
2021-04-12 21:07:08 +0800

    Update Solution.java

--- HashTable/18. 4Sum/Solution.java
+++ HashTable/18. 4Sum/Solution.java
@@ -1,12 +1,21 @@
 class Solution {
-    public List<List<Integer>> fourSum(int[] nums, int target) {
-		List<List<Integer>> ans = new ArrayList<List<Integer>>();
+	
+	public List<List<Integer>> fourSum(int[] nums, int target) {
+        int n = nums.length;
+        List<List<Integer>> ans = new ArrayList<List<Integer>>();
+        
+        if(nums.length < 4) 
+        	return ans;
         Arrays.sort(nums);
         
-        for (int i = 0; i < nums.length; i++) {
+        if ( nums[0]+nums[1]+nums[2]+nums[3] > target 
+        		||  nums[n-1]+nums[n-2]+nums[n-3]+nums[n-4] < target)
+			return ans;
+       
+        for (int i = 0; i < nums.length-3; i++) {
         	if (i > 0 && nums[i-1] == nums[i]) continue;
         	
-        	for (int j = i+1; j < nums.length; j++) {
+        	for (int j = i+1; j < nums.length-2; j++) {
         		if (j > i + 1 && nums[j] == nums[j-1]) continue;
         		
         		int l_ = j+1, r_ = nums.length-1;
