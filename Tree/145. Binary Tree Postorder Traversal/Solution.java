f37d3250c23075c4b1729eb2f807a62f1aef83d9
Wu,Pei-Yu <ts01174755@gmail.com>
2021-04-22 15:44:06 +0800

    Update Solution.java

--- Tree/145. Binary Tree Postorder Traversal/Solution.java
+++ Tree/145. Binary Tree Postorder Traversal/Solution.java
@@ -13,7 +13,24 @@
  *     }
  * }
  */
- 
+// 迭代解法
+class Solution {
+	public List<Integer> postorderTraversal(TreeNode root) {
+		List<Integer> ans = new ArrayList<Integer>();
+		Stack<TreeNode> st = new Stack<TreeNode>();
+		if(root == null) return ans;
+		
+		st.add(root);
+		while(!st.isEmpty()) {
+			TreeNode node = st.pop();
+			ans.add(node.val);
+			if (node.left != null)st.add(node.left);
+			if (node.right != null) st.add(node.right);
+		}
+        Collections.reverse(ans);
+		return ans;
+    }
+}
 // 圖論解法
 class Solution {
     public void PostOrderTraversal(TreeNode root, List<Integer> ans) {
