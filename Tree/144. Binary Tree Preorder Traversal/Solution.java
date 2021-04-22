444707dc9fda87b7358ee68ca6400c369520ca92
Wu,Pei-Yu <ts01174755@gmail.com>
2021-04-22 14:42:34 +0800

    Update Solution.java

--- Tree/144. Binary Tree Preorder Traversal/Solution.java
+++ Tree/144. Binary Tree Preorder Traversal/Solution.java
@@ -28,3 +28,21 @@ class Solution {
 		return ans;
     }
 }
+//迭代法
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+		List<Integer> ans = new ArrayList<Integer>();
+		Stack<TreeNode> st = new Stack<TreeNode>();
+		if(root == null) return ans;
+		
+		st.add(root);
+		while(!st.isEmpty()) {
+			TreeNode node = st.pop();
+			
+			ans.add(node.val);
+			if (node.right != null) st.add(node.right);
+			if (node.left != null)st.add(node.left);
+		}
+		return ans;
+    }
+}
