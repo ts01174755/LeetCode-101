2fa17afb4953498e489ec3bd07530f8d263c2c82
Wu,Pei-Yu <ts01174755@gmail.com>
2021-05-13 15:59:33 +0800

    Create Solution.java

---
+++ Tree/404. Sum of Left Leaves/Solution.java
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+   
+    public int sumOfLeftLeaves(TreeNode root) {        
+        if (root == null) return 0;
+        int midValue = 0;
+        if (root.left != null && root.left.left == null && root.left.right == null) {
+            midValue = root.left.val;
+        }
+        return midValue + sumOfLeftLeaves(root.left) + sumOfLeftLeaves(root.right);
+    }
+}
