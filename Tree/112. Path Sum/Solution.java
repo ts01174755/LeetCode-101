845cf20f40d3d1e7c3865a862f4d295458e055ee
Wu,Pei-Yu <ts01174755@gmail.com>
2021-05-17 12:25:27 +0800

    Update Solution.java

--- Tree/112. Path Sum/Solution.java
+++ Tree/112. Path Sum/Solution.java
@@ -13,6 +13,57 @@
  *     }
  * }
  */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+//堆疊法
+class Solution {
+    public class Node {
+        TreeNode root;
+        int sum;
+        public Node(TreeNode root, int sum) {
+            this.root = root;
+            this.sum = sum;
+        }
+        public String toString() {
+            return root.val + ", sum = " + sum;
+        }
+    }
+    
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        if (root == null) return false;
+        Stack<Node> st = new Stack<>();
+        
+        st.push(new Node(root, root.val));
+        while (!st.isEmpty()){
+            Node node = st.pop();
+            root = node.root;
+            int sum = node.sum;
+            if (root.left == null & root.right == null & sum == targetSum) 
+                return true;
+            
+            if (root.right != null) 
+                st.push(new Node(root.right, sum +  root.right.val));
+            if (root.left != null) 
+                st.push(new Node(root.left, sum +  root.left.val));
+        }
+        return false;
+        
+    }
+}
+
 class Solution {
     public boolean traversal(TreeNode node, int pathSum, int targetSum){
         if (node.left == null & node.right == null) {
